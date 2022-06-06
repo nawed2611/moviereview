b542444eab6fa3d9bce443d5913cb48eed23a9d5
nawed2611 <nawed2611@gmail.com>
2022-06-06 16:05:31 +0530

    Initial Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+
+    const handleClick = () => {
+
+    }
+
+  return (
+    <div>
+        <input type="text" placeholder="Enter a Movie" />
+        <button type="submit" onClick={handleClick}>Submit</button>
+        <pyscript src=""></pyscript>
+    </div>
+  )
+}
+
+export default Home;
